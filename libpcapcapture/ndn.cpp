18dbd64a29a2bda109b0e57f3fb7bd37d79aa079
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2018-12-23 16:16:47 +0800

    add

--- libpcapcapture/ndn.cpp
+++ libpcapcapture/ndn.cpp
@@ -171,7 +171,7 @@ void showpcap(const ptr_lib::shared_ptr<Data> &data) {
         vector<string> fileds;
         boost::split(fileds, name, boost::is_any_of("/"));
 
-        cout << "发raw socket 到: " << fileds[3] << endl;
+        cout << "发raw socket 到: " << fileds[3] << ", size=" << data->getContent().size()<< endl;
         rawSocketHelper.sendPacketTo(data->getContent().buf(), data->getContent().size(), fileds[3]);
     }
 }
