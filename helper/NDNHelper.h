37af032d3ac76db817418be5fb0532a5d6d6f97e
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2018-12-30 11:44:08 +0800

    白永杰模块对接（可编译，未测试）

--- helper/NDNHelper.h
+++ helper/NDNHelper.h
@@ -121,7 +121,7 @@ public: //静态变量
     static const string PREFIX_TCP_PRE_REQUEST;
     static const string PREFIX_TCP_REQUEST_DATA;
     static const string KEY_CONFIG_REGISTER_IP;
-	static const int NUM_OF_GUEST = 20;
+	static const int NUM_OF_GUEST = 5;
 private:
     Face face;
     MapCacheHelper<tuple_p> *cacheHelper;            //缓存表
