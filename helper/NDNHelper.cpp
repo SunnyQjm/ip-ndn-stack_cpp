55636ab9f2990b1efb2ebbb16240e89b7004e566
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2019-01-04 21:43:55 +0800

    降低Interest超时时间

--- helper/NDNHelper.cpp
+++ helper/NDNHelper.cpp
@@ -1,3 +1,5 @@
+#include <utility>
+
 //
 // Created by mingj on 18-12-22.
 //
@@ -228,8 +230,8 @@ void NDNHelper::onRegisterFailed(const Name &prefix) {
 }
 
 void NDNHelper::expressInterest(string name, bool isPre, bool isTCP) {
-	Interest interest(name);
-	interest.setInterestLifetime(2_s);	//兴趣报存活时间
+	Interest interest(std::move(name));
+	interest.setInterestLifetime(1_s);	//兴趣报存活时间
 //	cout << "express interest: " << name << endl;
     this->face.expressInterest(interest, bind(&NDNHelper::onData, this, _1, _2, isPre, isTCP),
                                bind(&NDNHelper::onNack, this, _1, _2), bind(&NDNHelper::onTimeout, this, _1, isPre));
