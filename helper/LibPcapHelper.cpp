bfe14a00f6169e5eccfd8b4a0113290b96abaf89
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2019-01-07 20:47:30 +0800

    删除多余的log

--- helper/LibPcapHelper.cpp
+++ helper/LibPcapHelper.cpp
@@ -50,9 +50,9 @@ void LibPcapHelper::handleRead(const boost::system::error_code &error) {
     auto tuple = std::get<0>(res);
     if (tuple != nullptr) {       //只传小于8000的块
         cout << tuple->size << endl;
-        if (tuple->size < 8800) {
+//        if (tuple->size < 8800) {
             this->deal(tuple);
-        }
+//        }
     }
     asyncRead();
 }
