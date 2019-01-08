bedeb69cbd19a7f2c63f9f61cb462d5ec93de8fe
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2019-01-08 15:19:21 +0800

    增大抓包缓存

--- helper/NDNHelper.h
+++ helper/NDNHelper.h
@@ -121,7 +121,7 @@ public: //静态变量
     static const string PREFIX_TCP_PRE_REQUEST;
     static const string PREFIX_TCP_REQUEST_DATA;
     static const string KEY_CONFIG_REGISTER_IP;
-	static const int NUM_OF_GUEST = 30;
+	static const int NUM_OF_GUEST = 25;
 private:
     Face face;
     MapCacheHelper<tuple_p> *cacheHelper;            //缓存表
