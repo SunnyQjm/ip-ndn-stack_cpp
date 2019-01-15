0927df6ecc01e94b8d4e0cacbfe672bef0e197a1
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2019-01-15 18:38:25 +0800

    添加辅助脚本

--- deployment/easy-ndn.sh
+++ deployment/easy-ndn.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 nodeName=$1
 ip=$2
+configPath=$3
 
 sudo ./createFace.sh ${nodeName}
 sudo ./start-nlsr.sh ${nodeName} &
 sleep 1
 sudo ./nlsr_advertise_helper.sh ${ip}
+sudo ../ip_ndn_stack_cpp ${configPath}
