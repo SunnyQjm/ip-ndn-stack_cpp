6a6b1d9cf70cda569f3e7c3e23e1ae821e5e5ba0
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2019-01-19 09:36:58 +0800

    更新拓扑，修改部署脚本

--- deployment/easy-ndn.sh
+++ deployment/easy-ndn.sh
@@ -2,9 +2,13 @@
 nodeName=$1
 ip=$2
 configPath=$3
+paramNum=$#
 
 sudo ./createFace.sh ${nodeName}
 sudo ./start-nlsr.sh ${nodeName} &
 sleep 1
 sudo ./nlsr_advertise_helper.sh ${ip}
-sudo ../ip_ndn_stack_cpp ${configPath}
+
+if [[ ${paramNum} -eq 3 ]]; then
+    sudo ../ip_ndn_stack_cpp ${configPath}
+fi
