2442783d46ea239903f7bdf95b729b7d7e7b708a
SunnyQjm <qjm3662@mail.dlut.edu.cn>
2019-01-20 11:15:55 +0800

    更新拓扑，修改部署脚本

--- helper/NDNHelper.h
+++ helper/NDNHelper.h
@@ -130,7 +130,7 @@ public: //静态变量
     static const string PREFIX_TCP_PRE_REQUEST;
     static const string PREFIX_TCP_REQUEST_DATA;
     static const string KEY_CONFIG_REGISTER_IP;
-	static const int NUM_OF_GUEST = 10;
+	static const int NUM_OF_GUEST = 100;
 private:
     Face face;
     MapCacheHelper<tuple_p> *cacheHelper;            //缓存表
